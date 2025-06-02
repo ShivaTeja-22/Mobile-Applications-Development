5e5466db7d9d17b3ac379482b00f12b738f47bb3
ShivaTeja-22 <Shivateja.panjala19@gmail.com>
2025-06-01 19:05:40 -0500

    assigmnet1

---
+++ Assingment_1/au hello world/au hello world/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  au hello world
+//
+//  Created by Auburn University Student on 6/1/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

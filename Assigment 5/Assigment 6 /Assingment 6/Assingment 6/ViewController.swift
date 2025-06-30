2973160831b4368e64427e1ffc0d2f8c5692ff11
ShivaTeja-22 <Shivateja.panjala19@gmail.com>
2025-06-29 23:12:31 -0500

    assigment 6

---
+++ Assigment 5/Assigment 6 /Assingment 6/Assingment 6/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Assingment 6
+//
+//  Created by Auburn University Student on 6/29/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Assigment 5/Assigment 6 /currency convertor/currency convertor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  currency convertor
+//
+//  Created by Auburn University Student on 6/29/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

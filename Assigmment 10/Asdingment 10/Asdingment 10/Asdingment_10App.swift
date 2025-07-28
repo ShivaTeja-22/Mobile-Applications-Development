959fbd236a710662a22e472ec2624db476af0151
ShivaTeja-22 <Shivateja.panjala19@gmail.com>
2025-07-27 20:23:08 -0500

    assigment 10

---
+++ Assigmment 10/Asdingment 10/Asdingment 10/Asdingment_10App.swift
@@ -0,0 +1,17 @@
+//
+//  Asdingment_10App.swift
+//  Asdingment 10
+//
+//  Created by Auburn University Student on 7/27/25.
+//
+
+import SwiftUI
+
+@main
+struct Asdingment_10App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Assigmment 10/Asdingment 10/Asdingment 10/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Asdingment 10
+//
+//  Created by Auburn University Student on 7/27/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
